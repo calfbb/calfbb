580670b7e47d79bf3c0f08534aeb292e0c3672cb
Rock  Lee <rock@RockdeMacBook-Pro.local>
2017-10-13 17:01:35 +0800

    删除多余文件

--- addons/db/controller/Index.php
+++ addons/db/controller/Index.php
@@ -1,6 +1,9 @@
 <?php
 /**
- * 示例控制器
+ * @className：插件应用路由文件
+ * @description：首页入口，文章页入口，公告页入口，用户中心入口
+ * @author:calfbb技术团队
+ * Date: 2017/10/13
  */
 namespace Addons\db\controller;
 
@@ -10,11 +13,32 @@ use  Framework\library\view;
 class Index
 {
     use view;
+    /**
+     * @var string $userName;用户名
+     */
+    public $userName;
+
+    /**
+     * 首页入口
+     * @return string
+     */
     public function index(){
 
         $this->display('index/index.html');
     }
 
-
+    /**
+     * 获取用户名字
+     * @param int  $id   用户的uid
+     * @param bool $status 用户状态
+     * @return string | bool  响应用户名
+     */
+    public function getUserName($uid,$status=true){
+            if($uid==66 && $status==true){
+                return $this->userName;
+            }else{
+                return false;
+            }
+    }
 
 }
